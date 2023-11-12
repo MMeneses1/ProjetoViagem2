0c6b23e69240fe8ee0e4e05ef4a05c4225fe67d3
Tamiris-Siqueira <tamiris.siqueira15@hotmail.com>
2023-11-12 20:47:44 -0300

    Estilizando tela de editar perfil

--- resources/views/insta/perfiledit.blade.php
+++ resources/views/insta/perfiledit.blade.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Perfil</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-</head>
-<body>
-    @include('inc.topo')
-
-    <div class="container">
-        <h1>Editar Perfil</h1>
-         
-        
-
+@extends('layouts.template-inside')
+@section('titulo', 'Atualizar Perfil')
+@section('css', '/css/perfiledit.css')
+@section('conteudo')
 
         @if (session('error'))
             <div class="alert alert-danger">
@@ -21,50 +9,34 @@
             </div>
         @endif
 
-        @if (session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
-
-        <form action="{{ route('perfil.update') }}" method="post" enctype="multipart/form-data">
+        <form action="{{ route('perfil.update') }}" method="post" enctype="multipart/form-data" class = "formularioeditar">
+            <h4>Atualize suas informações:</h4>
             @csrf
-            <!-- Resto do seu formulário -->
-
             <div class="form-group">
-                <label for="username">Nome de Usuário:</label>
-                <input type="text" id="username" name="username" value="{{ old('username', Auth::user()->username) }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="sexo">Sexo:</label>
-                <input type="text" id="sexo" name="sexo" value="{{ old('sexo', Auth::user()->sexo) }}">
+                <label for="username">Usuário:</label>
+                <input class="form-control" type="text" id="username" name="username" value="{{ old('username', Auth::user()->username) }}" required>
             </div>
 
             <div class="form-group">
                 <label for="telefone">Telefone:</label>
-                <input type="text" id="telefone" name="telefone" value="{{ old('telefone', Auth::user()->telefone) }}">
+                <input class="form-control" type="text" id="telefone" name="telefone" value="{{ old('telefone', Auth::user()->telefone) }}">
             </div>
 
             <div class="form-group">
                 <label for="foto_perfil">Foto de Perfil:</label>
-                <input type="file" id="foto_perfil" name="foto_perfil">
+                <input class="form-control" type="file" id="foto_perfil" name="foto_perfil">
             </div>
 
             <div class="form-group">
                 <label for="biografia">Biografia:</label>
-                <textarea id="biografia" name="biografia">{{ old('biografia', Auth::user()->biografia) }}</textarea>
+                <textarea class="form-control" rows = "2" id="biografia" name="biografia">{{ old('biografia', Auth::user()->biografia) }}</textarea>
             </div>
 
             <div class="form-group">
                 <label for="idioma">Idioma:</label>
-                <input type="text" id="idioma" name="idioma" value="{{ old('idioma', Auth::user()->idioma) }}">
+                <input class="form-control" type="text" id="idioma" name="idioma" value="{{ old('idioma', Auth::user()->idioma) }}">
             </div>
 
-            <button type="submit" class="btn btn-primary">Atualizar Perfil</button>
-
-            <a href="{{ route('perfilpessoal') }}" class="btn btn-primary">Voltar</a>
+            <button type="submit" class="btn btn-success">Atualizar Perfil</button>
         </form>
-    </div>
-</body>
-</html>
+@endsection
