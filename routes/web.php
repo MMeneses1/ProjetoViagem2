449fd745a3d5f1d2b070c51a0656ad49b2b690c6
Tamiris-Siqueira <tamiris.siqueira15@hotmail.com>
2023-11-15 20:41:03 -0300

    Organizando rotas

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,15 @@ Route::post('/login', [AuthController::class, 'authenticate']);
 Route::get('/signup', [MVPController::class, 'showRegisterForm'])->name('insta.register');
 Route::post('/signup', [MVPController::class, 'register'])->name('register');
 
-
-
 Route::middleware('auth')->group(function () {
     // Rotas protegidas por autenticação
-    
-Route::get('/perfil', [MVPController::class, 'showProfile'])->name('perfil');
-Route::post('/perfilpessoal', [MVPController::class, 'updateDados'])->name('perfilpessoal.update');
-Route::post('/logout', [MVPController::class, 'logout'])->name('logout');
-Route::get('/perfil/editar', [MVPController::class, 'showProfileEditForm'])->name('insta.perfiledit');
-Route::post('/perfil/editar', [MVPController::class, 'updateProfile'])->name('perfil.update');
-Route::get('/perfil/{username?}', [MVPController::class, 'showOtherUserProfile'])->name('perfil.outro');
-Route::get('/perfil/{loadedPosts?}', [MVPController::class, 'showProfile'])->name('perfil');
-
-
-
-  
+    Route::get('/perfil', [MVPController::class, 'showProfile'])->name('perfil');
+    Route::post('/perfilpessoal', [MVPController::class, 'updateDados'])->name('perfilpessoal.update');
+    Route::post('/logout', [MVPController::class, 'logout'])->name('logout');
+    Route::get('/perfil/editar', [MVPController::class, 'showProfileEditForm'])->name('insta.perfiledit');
+    Route::post('/perfil/editar', [MVPController::class, 'updateProfile'])->name('perfil.update');
+    Route::get('/perfil/{username?}', [MVPController::class, 'showOtherUserProfile'])->name('perfil.outro');
+    Route::get('/perfil/{loadedPosts?}', [MVPController::class, 'showProfile'])->name('perfil');
 
     Route::post('/post', [PostController::class, 'store'])->name('post.store');
     Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.delete');
@@ -41,24 +34,16 @@ Route::get('/perfil/{loadedPosts?}', [MVPController::class, 'showProfile'])->nam
     Route::get('/feed', [InicioController::class, 'index'])->name('feed');
     Route::get('/feed/{loadedPosts?}', [InicioController::class, 'index'])->name('feed');
     Route::get('/load-more-posts/{loadedPosts}', [InicioController::class, 'loadMorePosts'])->name('load.more.posts');
-
-
     
     Route::get('/pesquisa', [MVPController::class, 'pesquisar'])->name('pesquisa');
-
-
 
     Route::get('/pesquisa', [MVPController::class, 'pesquisar'])->name('pesquisa');
     Route::get('/pesquisa-ao-digitar', [MVPController::class, 'pesquisarAoDigitar'])->name('pesquisa.ao.digitar');
 
-
     Route::post('/perfil/seguir/{username}', [MVPController::class, 'followUser'])->name('perfil.seguir');
     Route::post('/perfil/deixar-de-seguir/{username}', [MVPController::class, 'unfollowUser'])->name('perfil.deixar-de-seguir');
-
-   
+    
     Route::get('/get-followers', [MVPController::class, 'getFollowers'])->name('get.followers');
     Route::get('/get-following', [MVPController::class, 'getFollowing'])->name('get.following');
-
-
 
 });
