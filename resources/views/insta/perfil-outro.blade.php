dbcbe38415dc4a691b581bd2bf2363b2ad9cc0dd
Tamiris-Siqueira <tamiris.siqueira15@hotmail.com>
2023-11-15 21:12:19 -0300

    Padronizando layout de perfil.outro.blade

--- resources/views/insta/perfil-outro.blade.php
+++ resources/views/insta/perfil-outro.blade.php
@@ -1,85 +1,72 @@
-
-
-@section('css', '/css/login.css')
+@extends('layouts.template-inside')
+@section('titulo', '$otherUser->username')
+@section('css', '/css/perfil.css')
 @section('conteudo')
-    <div class="container-fluid mx-auto">
-        <div class="row">
-            <a href="{{ route('feed') }}" class="btn btn-primary">Ir para o Início</a>
-        </div>
 
-        <div class="col-lg-12 mx-auto text-center">
-            <h1>Perfil de {{ $otherUser->username }}</h1>
-
-            @if($otherUser->foto_perfil)
-                <div class="profile-picture">
-                    <img src="{{ asset($otherUser->foto_perfil) }}" alt="Foto de Perfil" class="profile-image">
-                </div>
-            @endif
-            @if(auth()->user()->isFollowing($otherUser))
-    <!-- Botão para deixar de seguir -->
-    <form action="{{ route('perfil.deixar-de-seguir', ['username' => $otherUser->username]) }}" method="POST">
-        @csrf
-        <button type="submit">Deixar de Seguir</button>
-    </form>
-@elseif(auth()->user()->id !== $otherUser->id)
-    <!-- Botão para seguir -->
-    <form action="{{ route('perfil.seguir', ['username' => $otherUser->username]) }}" method="POST">
-        @csrf
-        <button type="submit">Seguir</button>
-    </form>
-@endif
-
-
-            <div class="profile-info">
-                <p><strong>Nome de Usuário:</strong> {{ $otherUser->username }}</p>
-
-                <div class="user-details">
-                    <p><strong>Nome:</strong> {{ $otherUser->nome }}</p>
-                    <p><strong>País:</strong>
-                        @if(empty($otherUser->pais))
-                            Não disponível
-                        @else
-                            {{ $otherUser->pais }}
-                        @endif
-                    </p>
-                </div>
-
-                <p><strong>Biografia:</strong>
-                    @if(empty($otherUser->biografia))
-                        Não disponível
-                    @else
-                        {{ $otherUser->biografia }}
-                    @endif
-                </p>
+    <div class = "profileinfo">
+        @if(Auth::user()->foto_perfil)
+            <div class="profilepicture">
+                <img src="{{ asset($otherUser->foto_perfil) }}" alt="Foto de Perfil" class="profileimage">
             </div>
-        </div>
-
-        @if(!$posts->isEmpty())
-            <h2>Posts de {{ $otherUser->username }}</h2>
-            <ul class="post-list">
-                @foreach($posts as $post)
-                    <li class="post-item">
-                        <div class="post-container">
-                            <div class="post-header">
-                                <p><strong>Data da Postagem:</strong> {{ $post->created_at->diffForHumans() }}</p>
-                                <p><strong>Usuário:</strong> {{ $post->user->username }}</p>
-                            </div>
-
-                            <!-- Adicione apenas a visualização do post, sem capacidade de exclusão -->
-                            <div class="post-content">
-                                <p>{{ $post->content }}</p>
-
-                                @if($post->image)
-                                    <img src="{{ asset($post->image) }}" alt="Imagem da postagem">
-                                @endif
-                            </div>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
-        @else
-            <p>Nenhuma postagem encontrada.</p>
         @endif
+
+        <div class="userdetails">
+            <p class = "info">{{ $otherUser->name }} • {{ $otherUser->username }}</p>
+
+            @if($otherUser->pais)
+                <p class = "info">{{ $otherUser->pais }}</p>
+            @endif
+
+            @if($otherUser->biografia)
+                <p class = "info">{{ $otherUser->biografia }}</p>
+            @endif
+
+            @if(auth()->user()->isFollowing($otherUser))
+                <!-- Botão para deixar de seguir -->
+                <form action="{{ route('perfil.deixar-de-seguir', ['username' => $otherUser->username]) }}" method="POST">
+                    @csrf
+                    <button class = "btn btn-outline-success" type="submit">Deixar de Seguir</button>
+                </form>
+                @elseif(auth()->user()->id !== $otherUser->id)
+                <!-- Botão para seguir -->
+                <form action="{{ route('perfil.seguir', ['username' => $otherUser->username]) }}" method="POST">
+                    @csrf
+                    <button class = "btn btn-success" type="submit">Seguir</button>
+                </form>
+            @endif
+        </div>
     </div>
-</body>
-</html>
+
+    @if(!$posts->isEmpty())
+    <ul role="list" class="postslista">
+        @foreach($posts->sortByDesc('created_at') as $post)
+            <li class="postitem">
+                <div class="conteudopost">
+                    <div class="card post">
+                        <div class="card-header header">
+                            <span class="userpost">
+                                @if($otherUser->foto_perfil)
+                                    <img src="{{ asset($otherUser->foto_perfil) }}" alt="Foto de Perfil" class="perfilfeed">
+                                @endif
+
+                                <a class = "linkperfil" href="{{ route('perfil') }}">{{ $post->user->username }}</a> • {{ $post->created_at->diffForHumans() }}
+                            </span>
+                        </div>
+                        <div class="card-body">
+                            <p class="textopost">{{ $post->content }}</p>
+                            @if($post->image)
+                                <div class="imagempost">
+                                    <img src="{{ asset($post->image) }}" alt="Imagem da postagem">
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+    @else
+        <p>Nenhuma postagem encontrada.</p>
+    @endif
+
+@endsection
